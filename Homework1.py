219c187dfc3f69a47ce5c6817f85ee320a3226fd
Greg Dews <gdews@msudenver.edu>
2019-08-28 13:03:38 -0600

    still relearning python syntax

--- Homework1.py
+++ Homework1.py
@@ -11,9 +11,9 @@ img = cv2.imread("NyanCat.PNG")
 length = img.shape[0]
 width = img.shape[1]
 
-for i in range(length)
-    for j in range(width)
-        if (math.sqrt((i**2)+(j**2)) <= 80)
+for i in range(length):
+    for j in range(width):
+        if (math.sqrt((i**2)+(j**2)) <= 80):
             img.item(i,j,0) = 255
             img.item(i,j,1) = 255
             img.item(i,j,2) = 255
