7f053341f3bef6ea199268da68bd02ec5321cc20
Greg Dews <gdews@msudenver.edu>
2019-08-29 21:23:57 -0600

    Greyscale, image module practice

--- Homework1.py
+++ Homework1.py
@@ -4,18 +4,21 @@ CS390S Digital Image Processing
 
 Homework 1
 """
-import numpy as np
-import cv2
+from PIL import Image
+import math
 
-img = cv2.imread("NyanCat.PNG")
-length = img.shape[0]
-width = img.shape[1]
+im = Image.open("NyanCat.PNG")
+nyanpixels = im.load() # create the pixel map
+img = Image.new( 'L', (im.size[1],im.size[0]), "white")
+imgpixels = img.load()
+length = img.size[1]
+width = img.size[0]
 
-for i in range(length):
-    for j in range(width):
-        if (math.sqrt((i**2)+(j**2)) <= 80):
-            img.item(i,j,0) = 255
-            img.item(i,j,1) = 255
-            img.item(i,j,2) = 255
+for i in range(width):    # for every col:
+    for j in range(length):    # For every row
+        if (math.sqrt( ( (i-(width/2))**2) + ( (j-(length/2))**2)) <= 80):
+            imgpixels[i,j] = nyanpixels[i,j][0]
+        else:
+            imgpixels[i,j] = (0)
 
 img.show()
