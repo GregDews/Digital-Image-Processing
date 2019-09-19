6cc12b2c5e76e6f28e34e42154b25b75588dc823
Greggers <gdews@msudenver.edu>
2019-09-19 16:22:05 -0600

    Added pic of autumn, added filters

--- HW2.py
+++ HW2.py
@@ -11,14 +11,44 @@ Homework 1
     pupil should be ~35-45 pixels
     use thresholding and mask out noise in background
 """
+import PIL
 from PIL import Image
 
-# Load image, Create the pixel map
-im = Image.open("iris.bmp").convert('LA')
-iris_pixels = im.load()
+# Load images, Create the pixel map
+iris = Image.open("iris.bmp").convert('LA')
+#iris_pixels = im.load()
+autumn = Image.open("PrincessAutumn.jpg").convert('LA')
+sobel_autumn = Image.new('L', autumn.size)
+laplacian_autumn = Image.new('L', autumn.size)
+median_autumn = Image.new('L', autumn.size)
 
-# Basic info
-length = im.size[1]
-width = im.size[0]
+# Create kernels
+sobelx = PIL.ImageFilter.Kernel((3,3),
+(-1,0,1,-2,0,2,-1,0,1)
+,1,0)
+sobely = PIL.ImageFilter.Kernel((3,3),
+(1,0,-1,2,0,-2,1,0,-1)
+,1,0)
+laplacian = PIL.ImageFilter.Kernel((3,3),
+(0,-1,0,-1,4,-1,0,-1,0)
+,1,0)
+median = PIL.ImageFilter.Kernel((3,3),
+(1,1,1,1,1,1,1,1,1)
+,1,0)
 
-# What's next
+# Apply filters
+horizontal = autumn.filter(sobelx)
+#vertical = autumn.filter(sobely)
+#sobel_autumn = horizontal + vertical
+laplacian_autumn = autumn.filter(laplacian)
+median_autumn = autumn.filter(median)
+
+# Save Pictures
+sobel_autumn.save("SobelAutumn.png")
+laplacian_autumn.save("LaplacianAutumn.png")
+median_autumn.save("MedianAutumn.png")
+
+# Show pictures
+sobel_autumn.show
+laplacian_autumn.show
+median_autumn.show
