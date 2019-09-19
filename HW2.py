c64cd51f2bcea4e43c89729c1c49aa3a80d4f821
Greggers <gdews@msudenver.edu>
2019-09-19 14:21:07 -0600

    Edit description of project, update open line

--- HW2.py
+++ HW2.py
@@ -3,20 +3,22 @@ Greg Dews
 CS390S Digital Image Processing
 
 Homework 1
-1. Load an image
-2. Create a grey-scale image
-3. Edit individual pixels using a for-loop to add a round frame
-4. Save image for report
+1. Load an image (self made)
+2. Convery to grey-scale image
+3. Apply filters: Sobel, Laplacian, Median
+4. Create your own filter not listed
+5. Using iris.bmp, detect edge of pupil
+    pupil should be ~35-45 pixels
+    use thresholding and mask out noise in background
 """
 from PIL import Image
-import math
 
 # Load image, Create the pixel map
-im = Image.open("iris.bmp")
+im = Image.open("iris.bmp").convert('LA')
 iris_pixels = im.load()
 
 # Basic info
-length = img.size[1]
-width = img.size[0]
+length = im.size[1]
+width = im.size[0]
 
 # What's next
