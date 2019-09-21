cc91aec4aa56dc08c4a854f1c76f6020824b0ae8
Greg Dews <gdews@msudenver.edu>
2019-09-21 15:20:46 -0600

    Rewrite using ndimage

--- HW2.py
+++ HW2.py
@@ -12,36 +12,45 @@ Homework 1
     use thresholding and mask out noise in background
 """
 from PIL import Image, ImageFilter
+from scipy import ndimage
+import numpy as np
 
 # Load images, Create the pixel map
 iris = Image.open("iris.bmp").convert('L')
 #iris_pixels = im.load()
 autumn = Image.open("PrincessAutumn.jpg").convert('L')
 autumn.save("GreyAutumn.jpg")
-sobel_autumn = Image.new("L", autumn.size)
-laplacian_autumn = Image.new("L", autumn.size)
-median_autumn = Image.new("L", autumn.size)
 
-# Create kernels
-sobelx = ImageFilter.Kernel((3,3),
-(-1, 0, 1,
- -2, 0, 2,
- -1, 0, 1),0,0)
+img = ndimage.imread("GreyAutumn.jpg")
+
+# sobel
+sobel_weights = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]])
+
+out = ndimage.convolve(img, sobel_weights)
+sobel_autumn = Image.fromarray(out, "L")
+sobel_autumn.save("SobelAutumn.jpg")
+"""
+# laplacian
+#laplacian_autumn = Image.new("L", autumn.size)
 laplacian = ImageFilter.Kernel((3,3),
 (-1,-1, -1,
  -1, 8, -1,
- -1,-1, -1))
+ -1,-1, -1),0,0)
+laplacian_autumn = autumn.filter(filter= laplacian)
+laplacian_autumn = ImageOps.equalize(laplacian_autumn)
+laplacian_autumn.save("LaplacianAutumn.jpg")
+tester = laplacian_autumn.filter(ImageFilter.MedianFilter)
+tester.save("laplacian_test.jpg")
+
+# median
+#median_autumn = Image.new("L", autumn.size)
 median = ImageFilter.Kernel((3,3),
 ( 1, 1, 1,
   1, 1, 1,
-  1, 1, 1))
-
-# Apply filters
-sobel_autumn = autumn.filter(filter= sobelx)
-laplacian_autumn = autumn.filter(filter= laplacian)
+  1, 1, 1),9,0)
 median_autumn = autumn.filter(filter= median)
-
-# Save Pictures
-sobel_autumn.save("SobelAutumn.jpg")
-laplacian_autumn.save("LaplacianAutumn.jpg")
 median_autumn.save("MedianAutumn.jpg")
+"""
