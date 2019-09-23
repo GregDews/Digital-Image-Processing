239416c4436504f44367fec004188dcb9dfac7c1
Greg Dews <gdews@msudenver.edu>
2019-09-23 15:10:16 -0600

    Added median filter using buffer

--- HW2.py
+++ HW2.py
@@ -58,13 +58,37 @@ out = cv2.filter2D(img, -1, laplacian_weights)
 laplacian_autumn = Image.fromarray(out)
 laplacian_autumn.save("LaplacianAutumn.jpg")
 
+# median filter
+#Pad image with copy of bordering pixels,
+ #   iterate through and find median of neighborhood
+padded = cv2.copyMakeBorder(
+    img, 1, 1, 1, 1, cv2.BORDER_REFLECT, None)
+width = len(padded) - 1
+length = len(padded[0]) - 1
+pix = [0]*9
+for i in range(1,width):
+    for j in range(1,length):
+        pix[0] = padded[i-1][j-1]
+        pix[1] = padded[i][j-1]
+        pix[2] = padded[i+1][j-1]
+        pix[3] = padded[i-1][j]
+        pix[4] = padded[i][j]
+        pix[5] = padded[i+1][j]
+        pix[6] = padded[i-1][j+1]
+        pix[7] = padded[i][j+1]
+        pix[8] = padded[i+1][j+1]
+        img[i-1][j-1] = sorted(pix)[4]
+median_autumn = Image.fromarray(img)
+median_autumn.save("median_autumn.jpg")
+
 # iris processing
-iris = cv2.imread( "iris.bmp", 0)
-out_not_x = cv2.filter2D(iris,-1, sobelx_weights * (-1))
-out_x = cv2.filter2D(iris,-1, sobelx_weights)
-out_not_y = cv2.filter2D(iris,-1, sobely_weights * (-1))
-out_y = cv2.filter2D(iris,-1, sobely_weights)
-filtered = out_x + out_not_x + out_y + out_not_y
-processed = cv2.filter2D(filtered, -1, average_weights)
-processed_iris = Image.fromarray(processed, 'L')
+iris = cv2.imread("iris.bmp", 0)
+blurred = cv2.filter2D(iris, -1, average_weights)
+out_not_x = cv2.filter2D(blurred,-1, sobelx_weights * (-1))
+out_x = cv2.filter2D(blurred,-1, sobelx_weights)
+out_not_y = cv2.filter2D(blurred,-1, sobely_weights * (-1))
+out_y = cv2.filter2D(blurred,-1, sobely_weights)
+filtered = out_x + out_y + out_not_y + out_not_x
+#wasted_var, processed = cv2.threshold(filtered, 10, 10, cv2.THRESH_OTSU)
+processed_iris = Image.fromarray(filtered, 'L')
 processed_iris.save("ProcessedIris.jpg")
