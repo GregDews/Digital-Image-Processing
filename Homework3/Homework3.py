f2e0adb1227c850dbda65ebff836c7de8213fc0c
Greg Dews <gdews@msudenver.edu>
2019-10-03 21:40:03 -0600

    trying to get histograms working

--- Homework3/Homework3.py
+++ Homework3/Homework3.py
@@ -13,20 +13,34 @@ To-Do: (suggested)
 
 """
 from PIL import Image
+from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 
+
+def imadjust(src, dst, tol, channel, vIn = (0,255), vOut = (0,255)):
+    rows, cols = src.shape
+    cv2.calcHist(src, [channel], None, [256], [0,255])
+    # Stretching
+    for r in range(rows):
+        for c in range(cols):
+            vs = max(src(r, c) - vIn[0], 0)
+            vd = min(int(vs + 0.5) + vOut[0], vOut[1])
+            dst[r, c, channel] = vd
+
+
 # load the images
-img = np.read("face_dark.bmp")
+img = cv2.imread("face_dark.bmp")
 
 # convert to luv 
 luv = cv2.cvtColor(np.array(img).astype('float32')/255, cv2.COLOR_RGB2Luv)
 
 L = luv[0]
 
-range = cv2.max(cv2.max(L))
+range = np.amax(np.amax(L))
 L = L / range
-L2 = cv2.imadjust(L,[],[],0.6)
+L2 = L
+imadjust(L,L2,0.6,0)
 L2 = L2 * range
 L= L * range
 
@@ -36,3 +50,4 @@ cv2.waitkey(0)
 
 cv2.imshow(luv)
 cv2.waitkey(0)
+
