f945a4b9f09a2c660463f1ce895322cf500b1381
Greg Dews <gdews@msudenver.edu>
2019-10-09 15:36:15 -0600

    10-9 notes

---
+++ Homework4/Homework4.py
@@ -0,0 +1,18 @@
+"""
+Greg Dews
+CS390S
+Homework 4
+
+1. Generate 256x256 gray image with only 2 intensities
+    0.4 and 0.7 of intensity
+    80 pixel radius circle
+
+2. Generate three types of noise
+        Gaussian mean=0 variance=0.1
+
+    add noise
+    histogram these images
+
+ACM event at 4pm in AES forum room
+
+"""
