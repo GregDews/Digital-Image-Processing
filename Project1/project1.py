b6df43984ea53deb3b8d4b5bc5cad88d7829e859
Greg Dews <gdews@msudenver.edu>
2019-10-25 11:11:24 -0600

    Project1, training complete

--- Project1/project1.py
+++ Project1/project1.py
@@ -29,13 +29,121 @@ testing---
 
 import numpy as np
 import cv2
+import glob
 from matplotlib import pyplot as plt
-from skimage.io import imread_collection
+from os import listdir
 from PIL import Image
 from scipy import linalg
 
 
-# Training - author Feng Jiang
+# Training - original author Feng Jiang
+"""
+call train with directory of images as param
+Will default to images/enrolling/*.bmp
+"""
+
+
+def train():
+    # Read in all Training images
+    # Build multi-D collections for Training Data
+
+    
+    # load all images in a directory
+    loaded_images = list()
+    X_filenames = list()
+    person_dict = dict()
+    num = 0
+    for filename in listdir('Project1/images/enrolling'):
+        # load image
+        if ".bmp" in filename:
+            img_data = cv2.imread('Project1/images/enrolling/' + filename, 0)
+            person = filename[0:-8]
+            if person not in person_dict:
+                person_dict[person] = []
+            person_dict[person].append(num)
+            num += 1
+            # store loaded image
+            loaded_images.append(img_data)
+            #print('> loaded %s %s' % (filename, img_data.shape))
+    
+    #print(person_dict)
+    all_images = [image[np.newaxis, ...] for image in loaded_images]
+
+    try:
+        X_arr = np.concatenate(all_images)
+    except ValueError:
+        raise ValueError('Image dimensions must agree.')
+
+    image_width = X_arr.shape[2]
+    image_height = X_arr.shape[1]
+    dims = (image_height, image_width)
+
+    vect_length = image_height*image_width
+    M = X_arr.shape[0]  # number of training images
+    Mp = 43  # number of  person
+
+    X_flat = convert_to_vects(X_arr)  # Convert images to vectors
+
+    m = len(person_dict.keys())
+
+    print('     Constructing dictionary of {} persons on {} training images...'.format(m, M))
+
+    # 2. build avg vector for each person !!!!
+    #  create parallel list to hold img names
+    print('     Creating average vector of images from each face...')
+    avg_vects = np.empty((Mp, vect_length), dtype=X_arr[0].dtype)
+    avg_vects_names = []
+    row = 0
+    for key, value in person_dict.items():
+        avg_vects_names.append(key)
+
+        temp_vects = np.empty((len(value), vect_length), dtype=X_arr[0].dtype)
+        for i in range(temp_vects.shape[0]):
+            # print('{} : {}'.format(key,self.X_flat[value[i]]))
+            temp_vects[i] = X_flat[value[i]]
+        # avg vectors for given image
+        avg_vects[row] = temp_vects.mean(axis=0)
+        row += 1
+    print('     Done...')
+
+    # 3. Calc mean vector of all persons in training set
+    print('     Calculating mean of all vectors...')
+    mean_vect = avg_vects.mean(axis=0)
+    print('     Done...')
+
+    print('     Subtracting mean from all average images...')
+    X_flat_centered = np.empty(avg_vects.shape, dtype=X_arr[0].dtype)
+    for i in range(Mp):
+        X_flat_centered[i] = center_vect(avg_vects[i], mean_vect)
+    X_flat_centered = X_flat_centered
+    print('     Done...')
+
+    print('     Computing eigenvalues and eigenvectors...')
+    cov_mat = np.cov(X_flat_centered)
+    # .eigh returns only real eigenvals
+    eigenvals, eigenvects = linalg.eigh(cov_mat.astype('float32'))
+
+    # reverse to put most contrib at beginning
+    ordered_eigenvals = eigenvals[::-1]
+    ordered_eigenvects = np.fliplr(eigenvects)
+    wt_A = np.dot(
+        ordered_eigenvects.T[:m].T, X_flat_centered)
+    #np.save('wt_A.npy', wt_A)
+    np.save('mean_vect.npy', mean_vect)
+
+    # Eigen Faces
+    row = wt_A.shape[0]
+    plt.figure()
+    for i in range(row):
+        plt.subplot(6, 8, i + 1)
+        plt.imshow(wt_A[i].reshape((image_height, image_width)), cmap=plt.cm.gray)
+        plt.title(avg_vects_names[i])
+        plt.xticks(())
+        plt.yticks(())
+        imgplot = plt.imshow(wt_A[i].reshape(
+            image_width, image_height), 'gray')
+    plt.show()
+
 
 def convert_to_vect(mat_):
 
@@ -65,30 +173,6 @@ def convert_to_vects(arr_):
         return arr_flat
 
 
-def build_person_dict(X):
-    '''
-    Assumes that filenames are in the format:
-        ID<2 digit person id>_<3 digit image number>.bmp
-        example: ID00_006.bmp
-
-    Input:
-            X: skimage imread_collection
-            collection of 2d images to build filename associations
-    '''
-    # extract filenames in order to build img averages
-    X_filenames = X.files
-
-    # Build dictionary of indices for each person
-    person_dict = dict()
-    for i in range(len(X_filenames)):
-        person = X_filenames[i][0:-8]
-        if person not in person_dict:
-            person_dict[person] = []
-        person_dict[person].append(i)
-
-    return person_dict
-
-
 def center_vect(vect, mean_vect):
     centered = vect - mean_vect
     centered = centered.clip(min=0)
@@ -96,72 +180,18 @@ def center_vect(vect, mean_vect):
     return centered
 
 
-# Read in all Training images from images/
-train_dir = "./images/enroll/*.bmp"
+def test():
 
-# Build multi-D collections for Training Data
-train = imread_collection(train_dir)
-X_arr = train.concatenate()
+    load_weight = np.load('wt_A.npy')
+    load_mean_vect = np.load('mean_vect.npy')
 
-image_width = X_arr.shape[2]
-image_height = X_arr.shape[1]
-dims = (image_height, image_width)
-
-vect_length = image_height*image_width
-M = X_arr.shape[0]  # number of training images
-Mp = 44  # number of  person
-
-X_flat = convert_to_vects(X_arr)  # Convert images to vectors
-
-person_dict = build_person_dict(train)
-m = len(person_dict.keys())
-
-print('     Constructing dictionary of {} persons on {} training images...'.format(m, M))
-
-# 2. build avg vector for each person !!!!
-#  create parallel list to hold img names
-print('     Creating average vector of images from each face...')
-avg_vects = np.empty((Mp, vect_length), dtype=X_arr[0].dtype)
-avg_vects_names = []
-row = 0
-for key, value in person_dict.items():
-    avg_vects_names.append(key)
-
-    temp_vects = np.empty((len(value), vect_length), dtype=X_arr[0].dtype)
-    for i in range(temp_vects.shape[0]):
-        # print('{} : {}'.format(key,self.X_flat[value[i]]))
-        temp_vects[i] = X_flat[value[i]]
-    # avg vectors for given image
-    avg_vects[row] = temp_vects.mean(axis=0)
-    row += 1
-print('     Done...')
-
-# 3. Calc mean vector of all persons in training set
-print('     Calculating mean of all vectors...')
-mean_vect = avg_vects.mean(axis=0)
-print('     Done...')
-
-print('     Subtracting mean from all average images...')
-X_flat_centered = np.empty(avg_vects.shape, dtype=X_arr[0].dtype)
-for i in range(Mp):
-    X_flat_centered[i] = center_vect(avg_vects[i], mean_vect)
-X_flat_centered = X_flat_centered
-print('     Done...')
-
-print('     Computing eigenvalues and eigenvectors...')
-cov_mat = np.cov(X_flat_centered.T)
-# .eigh returns only real eigenvals
-eigenvals, eigenvects = linalg.eigh(cov_mat)
-
-# reverse to put most contrib at beginning
-ordered_eigenvals = eigenvals[::-1]
-ordered_eigenvects = np.fliplr(eigenvects)
-
-weights_of_flat_centered_arr = np.dot(
-    X_flat_centered, ordered_eigenvects.T[:m].T)
-np.save('wt_A.npy', weights_of_flat_centered_arr)
-np.save('mean_vect.npy', mean_vect)
-
-
-load_weight = np.load('wt_A.npy')
-load_mean_vect = np.load('mean_vect.npy')
+if __name__ == "__main__":
+    response = input("train(1) or test(2)")
+    if "Train" in response or response is "1":
+        print("Training")
+        train()
+    elif "Test" in response or response is "2":
+        print("Testing")
+        test()
+    elif response:
+        print("did not match a command")
